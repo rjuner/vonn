367fd7b392597bc16f6a46870ca7c2dfe1abf152
Rhea Rivera <rjuner@gmail.com>
2016-10-07 17:33:04 -0500

    asdasd

--- config/auth.js
+++ config/auth.js
@@ -6,6 +6,6 @@ module.exports = {
   googleAuth: {
     clientID: '1013390006553-1j3uegqltoc5rj3c3aj5jlu58nh1o3tf.apps.googleusercontent.com',
     clientSecret: 'M8LxatEjOGvDwtB1cqO0Af96',
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    callbackURL: 'https://fierce-sea-19814.herokuapp.com/auth/google/callback'
   }
 };
